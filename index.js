b7eaab462373a1e063388c1851cb6aa72638a174
aqmeraamir <59936217+aqmeraamir@users.noreply.github.com>
2022-02-26 10:52:51 +0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+// Importing all the modules
+
+
+
+
+// Getting Time
+
+const date = new Date()
+let time = date.getTime()
+
+const minute = 1000 * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const year = day * 365;
+
+const d = new Date();
+let years = Math.round(d.getTime() / year);
+
+
+// Interaction
+
+// Server
